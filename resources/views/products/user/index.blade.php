273b44840b72248ea8dee556d143cd4234a4e0f1
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2024-06-07 20:42:06 +0700

    Digital License

--- resources/views/products/user/index.blade.php
+++ resources/views/products/user/index.blade.php
@@ -7,7 +7,7 @@
 @endsection
 @section('content')
     <div class="row">
-        <div class="col-lg-2">
+        {{-- <div class="col-lg-2">
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title mb-4">Filter</h4>
@@ -18,8 +18,8 @@
                     </ul>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-10">
+        </div> --}}
+        <div class="col-lg-12">
             <form action="" class="mt-4 mt-sm-0 float-sm-end d-sm-flex align-items-center">
                 <div class="search-box me-2">
                     <div class="position-relative">
@@ -38,7 +38,7 @@
                 @endforeach
             </div> --}}
             <div class="row mb-3">
-                @foreach ($categories as $category)
+                {{-- @foreach ($categories as $category)
                 <div class="col-xl-4 col-sm-6">
                     <div class="mt-2">
                         <h5>{{ $category->name }}</h5>
@@ -61,6 +61,26 @@
                         </div>
                     </div>
                     @endforeach
+                </div>
+                @endforeach --}}
+                @foreach ($products as $product)
+                <div class="col-xl-4 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="product-img position-relative">
+                                <img src="{{ URL::asset('product_digital/'.$product->image) }}" alt=""
+                                    class="img-fluid mx-auto d-block">
+                            </div>
+                            <div class="d-flex justify-content-between align-items-end mt-4">
+                                <div>
+                                    <h5 class="mb-3"><a href="{{ route('products.detail',['slug' => $product->slug, 'id' => $product->id]) }}" class="text-dark">
+                                            {{ $product->name }} </a></h5>
+                                    <h5 class="my-0"><span class="text-muted me-2">{{ 'Rp. '.number_format($product->price,0,',','.') }}</span></h5>
+                                    <h5 class="mt-1">Stock : <span>{{ $product->qty }}</span></h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 @endforeach
                 {{-- @foreach ($products as $product)
