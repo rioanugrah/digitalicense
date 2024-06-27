a52526ce1e834b6ddeca41ddc65a656198ff1fb7
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2024-06-27 21:42:21 +0700

    Digital License

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -54,7 +54,7 @@
                                     <td><img src="{{ asset('product_digital/'.$product->image) }}" width="200"></td>
                                     <td>
                                         <div>{{ $product->name }}</div>
-                                        <div>Kategori : {{ $product->category->name.' - '.$product->category->category_detail->name }}</div>
+                                        <div>Kategori : {{ $product->category->name.' - '.$product->category->category_detail->name == null ? '-' : $product->category->category_detail->name }}</div>
                                     </td>
                                     <td>{{ 'Rp. '.number_format($product->price,0,',','.') }}</td>
                                     <td>{{ $product->qty }}</td>
